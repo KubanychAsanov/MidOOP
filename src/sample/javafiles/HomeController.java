364a4afce2f1be5dba226c26795e949a64b8027e
Kubanych <kuba.asanov4@gmail.com>
2020-11-11 11:49:13 +0600

    some changes

--- src/sample/javafiles/HomeController.java
+++ src/sample/javafiles/HomeController.java
@@ -2,8 +2,17 @@ package sample.javafiles;
 
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 public class HomeController {
     @FXML
     private ImageView home_img;
+
+    @FXML
+    private WebView webView;
+
+    @FXML
+    void initialize() {
+    }
 }
